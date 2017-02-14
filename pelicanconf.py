0a311bb8c9d8f76bdbd018b01cbb4667b2d2a9c6
Dormouse Young <dormouse.young@gmail.com>
2017-02-14 16:54:34 +0800

    testing..

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,14 +38,18 @@ PLUGIN_PATHS = ['pelican-plugins']
 # pelican-bootstrap3 theme setting
 # More setting see:
 # https://github.com/getpelican/pelican-themes/tree/master/pelican-bootstrap3
-THEME = 'pelican-themes/pelican-bootstrap3'
+THEME = 'theme'
 
 CC_LICENSE = 'CC-BY-NC-ND'
 # GITHUB_USER = 'dormouse'
 DOCUTIL_CSS = True # for rst file
 JINJA_EXTENSIONS = ['jinja2.ext.i18n']
 
-PLUGINS = ['i18n_subsites', 'related_posts', 'tag_cloud', 'tipue_search']
+PLUGINS = ['i18n_subsites',
+           'related_posts',
+           'tag_cloud',
+           'tipue_search',
+           'pelican-toc']
 SHOW_DATE_MODIFIED = True
 DISPLAY_BREADCRUMBS = False
 DISPLAY_CATEGORY_IN_BREADCRUMBS = False
@@ -54,3 +58,11 @@ DISPLAY_TAGS_INLINE = True
 DISPLAY_CATEGORIES_ON_SIDEBAR = True
 DISPLAY_RECENT_POSTS_ON_SIDEBAR = False
 DIRECT_TEMPLATES = ('index', 'categories', 'authors', 'archives', 'search')
+
+TOC = {
+    'TOC_HEADERS' : '^h[1-6]',  # What headers should be included in the generated toc
+                                # Expected format is a regular expression
+
+    'TOC_RUN'     : 'true'      # Default value for toc generation, if it does not evaluate
+                                # to 'true' no toc will be generated
+}
